23afc6ef2c12ea5ee1d46691343f5e03c89df9e2
Petros Demetriou <dimitriupe@gmail.com>
2020-02-08 10:43:35 +0200

    React-Components-Insta-Clone project

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ Look at each file to see where you need to pass props or add code
 import React, { useState } from "react";
 import "./App.css";
 // import the PostsPage and SearchBar and add them to the App
-import "./components/PostsContainer/PostsPage.js"
-import "./components/SearchBar/SearchBarContainer.js"
+import SearchBar from "./components/SearchBar/SearchBarContainer.js"
+import PostsPage from "./components/PostsContainer/PostsPage.js"
 // import dummydata
-import "./dummy-data.js"
+import { dummyData } from "./dummy-data.js"
 
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
-  
+  const [dummyData, setDummyData] = useState(dummyData);
 
   return (
     <div className="App">
-      {/* Add components here  and pass props where appropriate */}
+      /* Add components here and pass props where appropriate */
+      <SearchBar/>
+      <PostsPage/>
+      
     </div>
   );
 };
